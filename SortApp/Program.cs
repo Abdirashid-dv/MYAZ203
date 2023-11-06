0177f6aae6997dab3489b31286e268efce0cba95
Abdurashid <Cabsho123@gmail.com>
2023-11-06 10:25:04 +0300

    Modified SortApp

--- SortApp/Program.cs
+++ SortApp/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var number = new List<int>();
+
+for (int i = 0; i < 6; i++)
+{
+    number.Add(i * 5);
+}
+
+System.Console.WriteLine($"Size of list:{number.Count}");
+
+foreach (var item in number)
+{
+
+}
