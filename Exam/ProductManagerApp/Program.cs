a1cce5dea1b2963c698faff02e7c91418521c5bc
Abdurashid <Cabsho123@gmail.com>
2023-11-24 22:56:23 +0300

    moved Exam folder to Week7

--- Exam/ProductManagerApp/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿
-using ProductManagerApp.Models;
-
-ProductManager productManager = new ProductManager();
-FoodProduct foodProduct = new FoodProduct(1, "Milk", 2.99, DateTime.Now.AddYears(1), true, 4.0);
-
-productManager.add(foodProduct);
-
-System.Console.WriteLine(productManager.getCount());
